aae4a3478953db71b04485f8f44eb085430e6d2f
Vincenzo Colonnella <folial@gmail.com>
2023-03-30 03:21:20 +0200

    Move scripts

--- get-secrets.sh
+++
@@ -1,4 +0,0 @@
-echo -n "ArgoCD: "
-kubectl get secret -n argocd argocd-initial-admin-secret --template={{.data.password}} | base64 --decode ; echo
-echo -n "Grafana: "
-kubectl get secret --namespace monitoring kube-prometheus-stack-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
--- port-forward.sh
+++
@@ -1,2 +0,0 @@
-kubectl port-forward svc/argocd-server -n argocd 8083:443 &> /dev/null &
-kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 10000:80 &> /dev/null &
